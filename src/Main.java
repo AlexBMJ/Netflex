2bbba9c5a1981721fd3c258d034ed9593479e0cc
AlexBMJ <AlexBMJ@users.noreply.github.com>
2020-12-11 05:09:53 +0100

    +added static elements for browse page +added images to resources

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import pages.BrowsePage;
 import pages.PageHandler;
@@ -6,6 +9,9 @@ import pages.PageHandler;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.getIcons().add(new Image("icon.png"));
         PageHandler window = PageHandler.create(stage, 10);
         window.addPage(new BrowsePage());
     }
--- src/pages/BrowsePage.java
+++ src/pages/BrowsePage.java
@@ -1,13 +1,21 @@
 package pages;
 
 
+import com.sun.javafx.scene.layout.region.Margins;
 import javafx.collections.ObservableList;
+import javafx.css.Stylesheet;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -15,39 +23,104 @@ public class BrowsePage implements Page {
     private Scene scene;
 
     public BrowsePage() {
-        Button backButton = new Button("<<< Back");
-        Button switchPage = new Button("Login Page");
-        Text text = new Text();
-        text.setFont(new Font(45));
-        text.setX(50);
-        text.setY(150);
-        text.setText("BROWSE PAGE");
+        final VBox root = new VBox();
+        root.setFillWidth(true);
+        root.setAlignment(Pos.TOP_LEFT);
+        root.setStyle("-fx-background-color: rgb(30,30,30);");
 
-        switchPage.setOnMouseClicked(mouseEvent -> {
-            PageHandler.get().addPage(new LoginPage());
-        });
+        // Header
+        final GridPane gp1 = new GridPane();
+        final ImageView menuButton = new ImageView("menu.png");
+        final ImageView logo = new ImageView("netflex_logo.png");
+        menuButton.setPreserveRatio(true);
+        menuButton.setFitHeight(40);
+        logo.setPreserveRatio(true);
+        logo.setFitHeight(40);
+        GridPane.setMargin(menuButton, new Insets(5,15,5,15));
+        GridPane.setMargin(logo, new Insets(5));
+        gp1.setPrefHeight(50);
+        gp1.setMinHeight(50);
+        gp1.setMaxHeight(50);
+        gp1.setStyle("-fx-background-color: black;");
+        gp1.add(menuButton,0,0);
+        gp1.add(logo,1,0);
+        root.getChildren().add(gp1);
 
-        backButton.setOnMouseClicked(mouseEvent -> {
-            try {
-                PageHandler.get().prevPage();
-            } catch (PageCacheException e) {
-                System.out.println(e.getMessage());
-            }
-        });
+        // Base
+        final StackPane stackPane = new StackPane();
+        final AnchorPane mainAnchorPane = new AnchorPane();
+        stackPane.setAlignment(Pos.TOP_CENTER);
+        mainAnchorPane.setTopAnchor(stackPane, 0d);
+        mainAnchorPane.setBottomAnchor(stackPane, 0d);
+        mainAnchorPane.setLeftAnchor(stackPane, 0d);
+        mainAnchorPane.setRightAnchor(stackPane, 0d);
+        mainAnchorPane.getChildren().add(stackPane);
+        root.getChildren().add(mainAnchorPane);
 
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.TOP_LEFT);
+        // Scroll Container
+        final ScrollPane scrollPane = new ScrollPane();
+        final AnchorPane ap1 = new AnchorPane();
+        final FlowPane flowPane = new FlowPane();
+        flowPane.setPadding(new Insets(20));
+        flowPane.setHgap(10);
+        flowPane.setVgap(10);
+        flowPane.setColumnHalignment(HPos.CENTER);
+        flowPane.setAlignment(Pos.TOP_CENTER);
+        flowPane.setStyle("-fx-background-color: rgb(30,30,30);");
+        FlowPane.setMargin(scrollPane, new Insets(20));
+        scrollPane.setContent(ap1);
+        scrollPane.setStyle("-fx-background-color: rgb(30,30,30);");
+        scrollPane.getStyleClass().add("scrollPane");
+        scrollPane.getStylesheets().add("scrollbar.css");
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        AnchorPane.setTopAnchor(flowPane, 0d);
+        AnchorPane.setBottomAnchor(flowPane, 0d);
+        AnchorPane.setLeftAnchor(flowPane, 0d);
+        AnchorPane.setRightAnchor(flowPane, 0d);
+        ap1.getChildren().add(flowPane);
+        stackPane.getChildren().add(scrollPane);
 
-        Group root = new Group();
-        root.getChildren().add(vbox);
-        ObservableList list = vbox.getChildren();
-        list.add(backButton);
-        list.add(switchPage);
-        list.add(text);
-        TextField tf = new TextField();
-        list.add(tf);
-        scene = new Scene(root, 600, 300);
-        tf.requestFocus();
+        for (int i=0; i<10; i++)
+            flowPane.getChildren().add(new ImageView("icon.png"));
+
+        // Search Field
+        final TextField searchField = new TextField();
+        searchField.setMaxWidth(300);
+        searchField.setMaxHeight(25);
+        searchField.setPromptText("Search");
+        searchField.setStyle("-fx-background-color: rgb(20,20,20); -fx-text-fill: white");
+        searchField.setOpacity(0.8f);
+        searchField.setAlignment(Pos.TOP_CENTER);
+        StackPane.setMargin(searchField, new Insets(15,0,0,0));
+        stackPane.getChildren().add(searchField);
+
+        // Filter Menu
+        final HBox leftSplit = new HBox();
+        final VBox menu = new VBox();
+        leftSplit.setMaxWidth(Region.USE_COMPUTED_SIZE);
+        leftSplit.setFillHeight(true);
+        leftSplit.setStyle("-fx-background-color: rgba(0,0,0,0.1)");
+        menu.setFillWidth(true);
+        menu.setPadding(new Insets(0,0,0,20));
+        menu.setStyle("-fx-background-color: black");
+        menu.setSpacing(10);
+        menu.setMaxWidth(200);
+        menu.setMinWidth(200);
+        menu.setAlignment(Pos.TOP_LEFT);
+        for (int i=0; i<10; i++) {
+            Label lbl = new Label(String.format("Item %s", i));
+            lbl.setTextFill(Color.WHITE);
+            lbl.setFont(new Font("Segoe UI Semibold", 24));
+            menu.getChildren().add(lbl);
+        }
+        leftSplit.getChildren().add(menu);
+        stackPane.getChildren().add(leftSplit);
+
+        // Loading Image
+        final ImageView loadingIcon = new ImageView("loading_medium.gif");
+
+        scene = new Scene(root);
     }
 
     public Scene getScene() {
